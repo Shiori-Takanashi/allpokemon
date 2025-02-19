cf58250c90c620d100a394a0b913388443236482
Takanashi-Shiori <bookmark8593@gmail.com>
2025-02-20 05:23:26 +0900

    基本構成

--- frontend/src/PokemonCards02/logic/fetchPokemons.ts
+++
@@ -1,8 +0,0 @@
-// logic/fetchPokemons.ts
-import axios from "axios";
-import { PokemonListResponse } from "../types/types";
-
-export async function fetchPokemons(apiUrl: string): Promise<PokemonListResponse> {
-  const response = await axios.get<PokemonListResponse>(apiUrl);
-  return response.data;
-}
